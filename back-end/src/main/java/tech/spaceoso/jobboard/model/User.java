70390a832a3370c7d1c18ac76c47860a8cf03907
Miguel Rico <miguelricodev@gmail.com>
2018-02-14 12:56:33 -0500

    adding database to app

--- back-end/src/main/java/tech/spaceoso/jobboard/model/User.java
+++ back-end/src/main/java/tech/spaceoso/jobboard/model/User.java
@@ -1,11 +1,26 @@
 package tech.spaceoso.jobboard.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
     String firstName;
     String lastName;
     String email;
     String password;
+
+    /**
+     * JPA requires this to be created because they use POJO's
+     */
+    protected User() {
+    }
 
     public User(Long id, String firstName, String lastName, String email, String password) {
         this.id = id;
